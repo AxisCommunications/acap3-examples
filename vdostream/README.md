e50f3fd20f576eac85ec2c2f7284aa2febe5adfa
Jeanette Löfgren <jeanettl@axis.com>
2020-06-08 11:15:08 +0200

    Added vdostream
    Change-Id: Ie1fa507827593cf486cbd29391f09a488cbfe8c4

---
+++ vdostream/README.md
@@ -0,0 +1,123 @@
+# An vdo stream based ACAP3 application on an edge device
+This readme file explains how to build an ACAP3 that uses the vdostream API. It is achived by using the containerized Axis API and toolchain images.
+
+Together with this file you should be able to find a directory called app, that directory contains the "vdoencodeclient.c" application which can easily be compiled and run with the help of the tools and step by step below.
+
+This example illustrates how to continuously capture frames from the vdo service, access the received buffer contents as well as the frame metadata. Captured frames are logged in the Application log.
+
+## Getting started
+These instructions below will guide you on how to execute the code. Below is the structure and scripts used in the example:
+
+```bash
+vdostream
+├── app
+│   ├── LICENSE
+│   ├── Makefile
+│   ├── vdoencodeclient.c
+├── Dockerfile
+└── README.md
+```
+* **Dockerfile**        -  Docker file with the specified Axis toolchain and API container to build the example specified
+* **LICENSE**           - Text file which lists all open source licensed source code distributed with the application.
+* **Makefile**          - Used by the make tool to build the program.
+* **README.md**         - Step by step instructions on how to run the example.
+* **vdoencodeclient.c** - Application to capture the frames using vdo service in C.
+
+### Limitations
+* The example is done for the armv7hf architecture, but it is possible to update to aarch64 aarch64.
+*  Supported video compression formats for an Axis video device are found in the datasheeet of the device.
+
+### How to run the code
+Below is a step by step on the whole process.
+
+#### Build and run the application
+Standing in your working directory run the following commands:
+```
+docker build --tag <APP_IMAGE> .
+```
+<APP_IMAGE> is the name to tag the image with, e.g., axisecp/vdoencodeclient:1.0
+
+Copy the build result from the container image to a local directory build
+```
+docker cp $(docker create <APP_IMAGE>):/opt/app ./build
+```
+
+Working dir now contains a build folder with the following files:
+```bash
+vdostream
+├── app
+│   ├── LICENSE
+│   ├── Makefile
+│   ├── vdoencodeclient.c
+├── Dockerfile
+└── README.md
+├── build
+│   ├── LICENSE
+│   ├── Makefile
+│   ├── package.conf
+│   ├── package.conf.orig
+│   ├── param.conf
+│   ├── vdoencodeclient*
+│   ├── vdoencodeclient_1_0_0_armv7hf.eap
+│   ├── vdoencodeclient_1_0_0_LICENSE.txt
+│   ├── vdoencodeclient.c
+```
+* **package.conf**                       - Defines the application and its configuration.
+* **package.conf.orig**                  - Defines the application and its configuration, original file.
+* **param.conf**                         - File containing application parameters.
+* **vdoencodeclient***                   - Application executable binary file.
+* **vdoencodeclient_1_0_0_armv7hf.eap**  - Application package .eap file.
+* **vdoencodeclient_1_0_0_LICENSE.txt**  - Copy of LICENSE file.
+
+#### Install your application.
+Installing your application on an Axis video device is as simple as:
+
+Browse to the following page (replace <axis_device_ip> with the IP number of your Axis video device):
+```
+http://<axis_device_ip>/#settings/apps
+```
+
+Click Add, the + sign and browse to the newly built vdoencodeclient_1_0_0_armv7hf.eap in build folder.
+
+Click Install, vdoencodeclient is now available as an application on the device.
+
+#### Run the application with default vdo parameters.
+
+Run the application by clicking on the application icon and enable the Start switch.
+
+Application log is found by:
+```
+http://<axis_device_ip>/axis-cgi/admin/systemlog.cgi?appname=vdoencodeclient
+```
+or by clicking on the App log text.
+
+#### Run application with different vdo parameters
+
+It is possible to run the application on the command line with different arguments, in the following order:
+* format
+  video compression format, h264 (default), h265 or jpeg
+* frames
+  number of frames
+* output
+  output filename
+
+Enable ssh on camera by:
+```
+http://<axis_device_ip>/axis-cgi/admin/param.cgi?action=update&Network.SSH.Enabled=yes
+```
+
+Start terminal:
+```
+ssh root@<axis_device_ip>
+```
+
+Run application from the filesystem on device:
+```
+cd /usr/local/packages/vdoencodeclient
+./vdoencodeclient --format h264 --frames 10 --output vdo.out
+```
+
+A file with the output (vdo.out) will be generated application folder, from where it can be extracted.
+
+## License
+**Apache License 2.0**
---
+++ vdostream/app/vdoencodeclient.c
@@ -0,0 +1,246 @@
+/**
+ * Copyright (C) 2019-2020, Axis Communications AB, LUND, SWEDEN
+ */
+
+/**
+ * - vdoencodeclient -
+ *
+ * This application is a basic vdo type of application.
+ *
+ * The application starts a vdo steam and then illustrates how to continuously
+ * capture frames from the vdo service, access the received buffer contents
+ * as well as the frame metadata.
+ *
+ * The application expects three arguments on the command line in the following
+ * order: format frames output.
+ *
+ * First argument, format, is a string describing the video compression format.
+ * Possible values are h264 (default), h265 and jpeg.
+ *
+ * Second argument, frames, is an integer for number of captured frames.
+ *
+ * Finally, the third argument, output, is the output filename.
+ *
+ * Suppose that you have done through the steps of installation.
+ * Then you would go to /usr/local/packages/vdoencodeclient on your device
+ * and then for example run:
+ *     ./vdoencodeclient \
+ *         --format h264 \
+ *         --frames 10 \
+ *         --output vdo.out
+ *
+ * or in short argument syntax
+ *      ./vdoencodeclient \
+ *         -t h264 \
+ *         -n 10 \
+ *         -o vdo.out
+ */
+
+#include "vdo-map.h"
+#include "vdo-error.h"
+#include "vdo-types.h"
+#include "vdo-stream.h"
+
+#include <stdlib.h>
+#include <signal.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+#include <syslog.h>
+
+#define VDO_CLIENT_ERROR g_quark_from_static_string("vdo-client-error")
+
+static VdoStream* stream;
+static gboolean shutdown = FALSE;
+static const gchar *param_desc = "";
+static const gchar *summary = "Encoded video client";
+
+// Facilitate graceful shutdown with CTRL-C
+static void
+handle_sigint(int signum)
+{
+    shutdown = TRUE;
+}
+
+// Determine and log the received frame type
+static void
+print_frame(VdoFrame* frame)
+{
+    if (!vdo_frame_get_is_last_buffer(frame))
+        return;
+
+    gchar *frame_type;
+    switch(vdo_frame_get_frame_type(frame)) {
+        case VDO_FRAME_TYPE_H264_IDR:
+        case VDO_FRAME_TYPE_H265_IDR:
+        case VDO_FRAME_TYPE_H264_I:
+        case VDO_FRAME_TYPE_H265_I:
+            frame_type = "I";
+            break;
+        case VDO_FRAME_TYPE_H264_P:
+        case VDO_FRAME_TYPE_H265_P:
+            frame_type = "P";
+            break;
+        case VDO_FRAME_TYPE_JPEG:
+            frame_type = "jpeg";
+            break;
+        default:
+            frame_type = "NA";
+    }
+
+    syslog(LOG_INFO, "frame = %4u, type = %s, size = %zu\n",
+            vdo_frame_get_sequence_nbr(frame),
+            frame_type,
+            vdo_frame_get_size(frame));
+}
+
+// Set vdo format from input parameter
+static gboolean
+set_format(VdoMap *settings, gchar *format, GError **error)
+{
+    if (g_strcmp0(format, "h264") == 0) {
+        vdo_map_set_uint32(settings, "format", VDO_FORMAT_H264);
+    } else if(g_strcmp0(format, "h265") == 0) {
+        vdo_map_set_uint32(settings, "format", VDO_FORMAT_H265);
+    } else if(g_strcmp0(format, "jpeg") == 0) {
+        vdo_map_set_uint32(settings, "format", VDO_FORMAT_JPEG);
+    } else {
+        g_set_error(error, VDO_CLIENT_ERROR, VDO_ERROR_NOT_FOUND,
+                    "Format \"%s\" is not supported\n", format);
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+/**
+ * Main function that starts a stream with the following options:
+ *
+ * --format [h264, h265, jpeg]
+ * --frames [number of frames]
+ * --output [output filename]
+ */
+int
+main(int argc, char* argv[])
+{
+    GError *error = NULL;
+    gchar *format = "h264";
+    guint frames = G_MAXUINT;
+    gchar *output_file = "/dev/null";
+    FILE* dest_f = NULL;
+
+    openlog(NULL, LOG_PID, LOG_USER);
+
+    GOptionEntry options[] = {
+        {"format", 't', 0, G_OPTION_ARG_STRING, &format, "format (h264, h265, jpeg)", NULL},
+        {"frames", 'n', 0, G_OPTION_ARG_INT, &frames, "number of frames", NULL},
+        {"output", 'o', 0, G_OPTION_ARG_FILENAME, &output_file, "output filename", NULL},
+        {NULL, 0, 0, 0, NULL, NULL, NULL,}
+    };
+
+    GOptionContext *context = g_option_context_new(param_desc);
+    if (!context)
+        return -1;
+
+    g_option_context_set_summary(context, summary);
+    g_option_context_add_main_entries(context, options, NULL);
+    if (!g_option_context_parse(context, &argc, &argv, &error))
+        goto exit;
+
+    dest_f = fopen(output_file, "wb");
+    if (!dest_f) {
+        g_set_error(&error, VDO_CLIENT_ERROR, VDO_ERROR_IO, "open failed: %m");
+        goto exit;
+    }
+
+    if (signal(SIGINT, handle_sigint) == SIG_ERR) {
+        g_set_error(&error, VDO_CLIENT_ERROR, VDO_ERROR_IO,
+                    "Failed to install signal handler: %m");
+        goto exit;
+    }
+
+    VdoMap *settings = vdo_map_new();
+    if (!set_format(settings, format, &error))
+        goto exit;
+
+    // Create a new stream
+    stream = vdo_stream_new(settings, NULL, &error);
+    g_clear_object(&settings);
+    if (!stream)
+        goto exit;
+
+    if (!vdo_stream_attach(stream, NULL, &error))
+        goto exit;
+
+    VdoMap *info = vdo_stream_get_info(stream, &error);
+    if (!info)
+        goto exit;
+
+    syslog(LOG_INFO, "Starting stream: %s, %ux%u, %u fps\n",
+            format,
+            vdo_map_get_uint32(info, "width", 0),
+            vdo_map_get_uint32(info, "height", 0),
+            vdo_map_get_uint32(info, "framerate", 0));
+
+    g_clear_object(&info);
+
+    // Start the stream
+    if (!vdo_stream_start(stream, &error))
+        goto exit;
+
+    // Loop until interrupt by Ctrl-C or reaching G_MAXUINT
+    for (guint n = 0; n < frames; ++n) {
+        // Lifetimes of buffer and frame are linked, no need to free frame
+        VdoBuffer* buffer = vdo_stream_get_buffer(stream, &error);
+        VdoFrame*  frame  = vdo_buffer_get_frame(buffer);
+
+        // Error occurred
+        if (!frame)
+            goto exit;
+
+        // SIGINT occurred
+        if (shutdown) {
+            vdo_stream_buffer_unref(stream, &buffer, NULL);
+            goto exit;
+        }
+
+        print_frame(frame);
+
+        gpointer data = vdo_buffer_get_data(buffer);
+        if (!data) {
+            g_set_error(&error, VDO_CLIENT_ERROR, 0, "Failed to get data: %m");
+            vdo_stream_buffer_unref(stream, &buffer, NULL);
+            goto exit;
+        }
+
+        if (!fwrite(data, vdo_frame_get_size(frame), 1, dest_f)) {
+            g_set_error(&error, VDO_CLIENT_ERROR, 0, "Failed to write frame: %m");
+            vdo_stream_buffer_unref(stream, &buffer, NULL);
+            goto exit;
+        }
+
+        // Release the buffer and allow the server to reuse it
+        if (!vdo_stream_buffer_unref(stream, &buffer, &error))
+            goto exit;
+    }
+
+exit:
+    // Ignore SIGINT and server maintenance
+    if (shutdown || vdo_error_is_expected(&error))
+        g_clear_error(&error);
+
+    gint ret = EXIT_SUCCESS;
+    if (error) {
+        syslog(LOG_INFO, "vdo-encode-client: %s\n", error->message);
+        ret = EXIT_FAILURE;
+    }
+
+    if (dest_f)
+        fclose(dest_f);
+
+    g_clear_error(&error);
+    g_clear_object(&stream);
+
+    g_option_context_free(context);
+
+    return ret;
+}
