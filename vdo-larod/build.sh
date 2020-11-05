7eb785c46d6e0472b7615b13496cb9b1f03321af
Mirza Mahmutovic <mirzam@axis.com>
2020-11-05 16:00:19 +0100

    Update Docker files
    Update Docker files in examples with argument REPO
    
    Change-Id: I7ae1d56003c9b0ce4f6193b807a2e4d2506a0d7a

--- vdo-larod/build.sh
+++ vdo-larod/build.sh
@@ -11,7 +11,10 @@ pushd yuv || exit 1
 ./build.sh "$UBUNTU_VERSION"
 popd || exit 1
 
-docker build --build-arg http_proxy="${http_proxy}" --build-arg https_proxy="${https_proxy}" --tag "$1" .
+[ -n "$REPO" ] && repo="--build-arg REPO=$REPO"
+[ -n "$VERSION" ] && version="--build-arg VERSION=$VERSION"
+
+docker build --build-arg http_proxy="${http_proxy}" --build-arg https_proxy="${https_proxy}" $repo $version --tag "$1" .
 
 rm -rf build/
 docker cp $(docker create "$1"):/opt/app ./build
