0867c4a946b83f18e9e2190481db4b497a13867c
Johan Ahlkvist <joha@axis.com>
2021-01-28 16:46:42 +0100

    update tensorflow-to-larod to ubuntu 20.04, dockerfile optimizations

--- tensorflow-to-larod/run_env.sh
+++ tensorflow-to-larod/run_env.sh
@@ -1,6 +1,18 @@
+# Make sure a environment name was given
 if [ $# -lt 1 ]; then
     echo "Argument should be: <ENVIRONMENT_NAME>"
     exit 1
 fi
 
-docker run --gpus all -v /var/run/docker.sock:/var/run/docker.sock --network host --name "$1" -it tensorflow_to_larod /bin/bash
+ENV_NAME="$1"
+# If the --no-gpu option is set, we should empty the GPU_FLAG to disable GPU
+# acceleration in the container
+GPU_FLAG="--gpus all"
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    "--no-gpu") GPU_FLAG='' ;;
+  esac
+done
+
+docker run $GPU_FLAG -v /var/run/docker.sock:/var/run/docker.sock --network host --name $ENV_NAME -it tensorflow_to_larod /bin/bash
