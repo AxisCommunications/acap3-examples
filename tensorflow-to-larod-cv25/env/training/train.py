90484bb686e5ac6f83153ee53f8a8df66ae9f492
pataxis <pataxis@users.noreply.github.com>
2022-11-08 14:21:24 +0100

    Add new CV25 example tensorflow-to-larod-cv25

---
+++ tensorflow-to-larod-cv25/env/training/train.py
@@ -0,0 +1,58 @@
+"""
+ * Copyright (C) 2021 Axis Communications AB, Lund, Sweden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+"""
+
+""" train.py
+    Instantiates a data generator and a model and trains the model.
+
+    usage: train.py [-h] -i <path to dataset image dir> \
+        -a <path to dataset annotation json-file>
+"""
+
+import argparse
+import tensorflow as tf
+from model import create_model
+from utils import SimpleCOCODataGenerator as DataGenerator
+
+
+def train(image_dir, annotation_path):
+    """ Initiates a model and and trains it using a data generator. The model
+        is then saved to the output path.
+
+    Args:
+        image_dir (str): Path to the directory holding the dataset images.
+        annotation_path (str): Path to the dataset annotation json-file.
+    """
+    person_car_indicator = create_model()
+    person_car_indicator.compile(optimizer='adam', metrics=['binary_accuracy'],
+                                 loss=['bce', 'bce'])
+    person_car_indicator.summary()
+    data_generator = DataGenerator(image_dir, annotation_path, batch_size=16)
+    person_car_indicator.fit(data_generator, epochs=10)
+
+    tf.saved_model.save(person_car_indicator, 'models/saved_model')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train a basic model.')
+    parser.add_argument('-i', '--images', type=str, required=True,
+                        help='path to the directory containing training \
+                        images')
+    parser.add_argument('-a', '--annotations', type=str, required=True,
+                        help='path to the .json-file containing COCO instance \
+                        annotations')
+
+    args = parser.parse_args()
+    train(args.images, args.annotations)
---
+++ tensorflow-to-larod-cv25/run_env.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -e
+
+# Make sure a environment name was given
+[ $# -eq 1 ] || [ $# -eq 2 ] || [ $# -eq 3 ] || {
+    echo "Argument should be: <ENVIRONMENT_NAME> [--no-gpu] [--tag=<ENV_IMAGE>]"
+    exit 1
+}
+
+ENV_NAME=$1
+# If the --no-gpu option is set, we should empty the GPU_FLAG to disable GPU
+# acceleration in the container
+GPU_FLAG='--gpus all'
+TAG=tensorflow-to-larod-cv25
+for arg in "$@"; do
+  shift
+  case "$arg" in
+    --no-gpu) GPU_FLAG='' ;;
+    --tag=*) TAG=${arg##*=} ;;
+  esac
+done
+
+# shellcheck disable=SC2086 # Deliberately word split arguments
+docker run $GPU_FLAG -v /var/run/docker.sock:/var/run/docker.sock --network host --name $ENV_NAME -it $TAG /bin/bash
